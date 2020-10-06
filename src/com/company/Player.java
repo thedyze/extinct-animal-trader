8e618e5d56d73ac2971201be643e3d474dd9f4eb
thedyze <the.dyze@gmail.com>
2020-10-06 21:48:00 +0200

    added player names and cash to player class

---
+++ src/com/company/Player.java
@@ -0,0 +1,8 @@
+package com.company;
+
+import java.util.*;
+
+public class Player {
+    public static ArrayList<String> names = new ArrayList<>();
+    public static ArrayList<Integer> cash = new ArrayList<>();
+}
