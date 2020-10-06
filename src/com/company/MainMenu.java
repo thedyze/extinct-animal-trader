0210c9fee943858bd568dd18a18acedbc28e0237
thedyze <the.dyze@gmail.com>
2020-10-06 17:03:03 +0200

    can now select number of players and enter names

--- src/com/company/MainMenu.java
+++ src/com/company/MainMenu.java
@@ -3,11 +3,26 @@ import java.util.*;
 
 public class MainMenu {
 
+    public static int player = 1;
+
+
     public static void init() {
+
         Dialogs.clear();
-        System.out.println("      Welcome to: \n \u001B[1mEXTINCT ANIMAL TRADER\033[0;0m \n ----------------------\n" +
-                        "Press 1 for new game");
-        var age = Dialogs.promptInt(":>", 1, 1);
+        System.out.println("      Welcome to: \n \u001B[1mEXTINCT ANIMAL TRADER\033[0;0m \n ----------------------\n");
+        var input = Dialogs.promptInt("Press 1 for new game :>", 1, 1);
+        var numberOfPlayers = Dialogs.promptInt("How may players? (1-4)", 1, 4);
+        String [] pNames = new String [numberOfPlayers];
+
+        //loop through number of players and set names.
+        for (player = 1; player <= numberOfPlayers; player++ ) {
+            pNames[player-1] = Dialogs.prompt("What's your name player " + player + "?");
+
+        }
+        System.out.println("Welcome to the game ");
+        for (player = 0; player < numberOfPlayers; player++ ) {
+            System.out.println(pNames[player]);
+        }
     }
 
 }
