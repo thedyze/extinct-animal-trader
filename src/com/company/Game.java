be304add279a945b07ed7bcc51d3d53c10399b90
thedyze <the.dyze@gmail.com>
2020-10-06 22:03:43 +0200

    reformat

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -13,21 +13,22 @@ public class Game {
 
             for (player = 1; player <= numberOfPlayers; player++) {
                 System.out.println("Turn: " +currentTurn + "/" +totalTurns +
-                        ". Player: " + Player.names.get(player -1) + ". You have " + Player.cash.get(player -1) + "€.");
+                        ". Player " + Player.names.get(player -1) + ", you have " + Player.cash.get(player -1) + "€");
                 playerAction();
             }
     }
 
     public static void playerAction() {
-        var input = Dialogs.promptInt("\nChoose your action:\n 1.Visit Store 2.Feed Animals 3.Mate Animals"
+        var input = Dialogs.promptInt("\nChoose your action:\n [1:Visit Store] [2:Feed Animals] [3:Mate Animals]"
                 , 1, 3);
-        /*
+
         switch (input) {
             case 1: Store.menu();
-            case 2: feedAnimals();
-            case 3: mateAnimals();
+
+            case 2: //feedAnimals();
+            case 3: //mateAnimals();
         }
-         */
+
     }
 }
 
