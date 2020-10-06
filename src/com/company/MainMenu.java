99cf1b5a31d8f271ac190d01ca32af6f8f34c651
thedyze <the.dyze@gmail.com>
2020-10-06 18:37:32 +0200

    updated add names loop

--- src/com/company/MainMenu.java
+++ src/com/company/MainMenu.java
@@ -5,24 +5,28 @@ public class MainMenu {
 
     public static int player = 1;
     public static int totalTurns = 5;
-
+    public static int numberOfPlayers = 4;
+    public static Scanner scanner = new Scanner(System.in);
+    public static String [] pNames;
 
     public static void init() {
 
         Dialogs.clear();
         System.out.println("      Welcome to: \n \u001B[1mEXTINCT ANIMAL TRADER\033[0;0m \n ----------------------\n");
         var input = Dialogs.promptInt("Press 1 for new game :>", 1, 1);
-        var numberOfPlayers = Dialogs.promptInt("How may players? (1-4)", 1, 4);
-        String [] pNames = new String [numberOfPlayers];
+        numberOfPlayers = Dialogs.promptInt("How may players? (1-4)", 1, 4);
+        pNames = new String [numberOfPlayers];
+
 
         //loop through number of players and set names.
-        for (player = 1; player <= numberOfPlayers; player++ ) {
-            pNames[player-1] = Dialogs.prompt("What's your name player " + player + "?");
+        for (int i = 0; i < pNames.length; i++ ) {
+            System.out.println("Player "  + (i + 1) + " name?");
+            pNames[i] = scanner.nextLine();
         }
 
         System.out.println("Welcome to the game ");
-        for (player = 0; player < numberOfPlayers; player++ ) {
-            System.out.println(pNames[player]);
+        for (String pName : pNames) {
+            System.out.println(pName);
         }
         //select number of game turns
         totalTurns = Dialogs.promptInt("How many turns should the game be? (5-30)", 5,30);
