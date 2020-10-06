e67128cd56b546ffc1207b835732037cbba36941
thedyze <the.dyze@gmail.com>
2020-10-06 19:59:11 +0200

    moved public variables, arraylists from MainMenu to Game

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -3,16 +3,21 @@ import java.util.*;
 
 public class Game {
     public static int currentTurn = 0;
+    public static int totalTurns = 5;
+    public static int numberOfPlayers = 4;
+    public static int player = 1;
+    public static ArrayList<String> pNames = new ArrayList<>();
+    public static ArrayList<Integer> pCash = new ArrayList<>();
 
 
     public static void turn() {
-            if (currentTurn < MainMenu.totalTurns) {
+            if (currentTurn <totalTurns) {
                 currentTurn++;
             }
 
-            for (MainMenu.player = 1; MainMenu.player <= MainMenu.numberOfPlayers; MainMenu.player++) {
-                System.out.println("Turn:" +currentTurn + ", Player: " + MainMenu.pNames.get(MainMenu.player-1));
-                System.out.println("You have " + MainMenu.playerCash.get(MainMenu.player-1) + "€");
+            for (player = 1; player <= numberOfPlayers; player++) {
+                System.out.println("Turn: " +currentTurn + ", Player: " + pNames.get(player-1));
+                System.out.println("You have " + pCash.get(player-1) + "€");
             }
     }
 }
--- src/com/company/MainMenu.java
+++ src/com/company/MainMenu.java
@@ -2,35 +2,28 @@ package com.company;
 import java.util.*;
 
 public class MainMenu {
-
-    public static int player = 1;
-    public static int totalTurns = 5;
-    public static int numberOfPlayers = 4;
     public static Scanner scanner = new Scanner(System.in);
-    public static ArrayList<String> pNames = new ArrayList<>();
-    public static ArrayList<Integer> playerCash = new ArrayList<>();
-
 
     public static void init() {
 
         Dialogs.clear();
         System.out.println("      Welcome to: \n \u001B[1mEXTINCT ANIMAL TRADER\033[0;0m \n ----------------------\n");
         var input = Dialogs.promptInt("Press 1 for new game :>", 1, 1);
-        numberOfPlayers = Dialogs.promptInt("How may players? (1-4)", 1, 4);
+        Game.numberOfPlayers = Dialogs.promptInt("How may players? (1-4)", 1, 4);
 
         //loop through number of players and set names, add cash
-        for (int i = 0; i < numberOfPlayers; i++ ) {
+        for (int i = 0; i < Game.numberOfPlayers; i++ ) {
             System.out.println("Player "  + (i + 1) + " name?");
-            pNames.add (scanner.nextLine());
-            playerCash.add(10000);
+            Game.pNames.add (scanner.nextLine());
+            Game.pCash.add(10000);
         }
 
         System.out.println("Welcome to the game ");
-        for (String pName : pNames) {
+        for (String pName : Game.pNames) {
             System.out.println(pName);
         }
         //select number of game turns
-        totalTurns = Dialogs.promptInt("How many turns should the game be? (5-30)", 5,30);
+        Game.totalTurns = Dialogs.promptInt("How many turns should the game be? (5-30)", 5,30);
     }
 
 }
