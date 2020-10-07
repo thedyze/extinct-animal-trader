5b492c26f05bac7bc2cb48859c53ebda21cea4a5
thedyze <the.dyze@gmail.com>
2020-10-07 18:16:59 +0200

    testing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    //Game.init();
-	    //Game.turn();
-        Player.cash=10000;
-        Store.menu();
+	    Game.init();
+	    Game.turn();
+        //Player.cash=10000;
+        //Store.menu();
     }
 }
